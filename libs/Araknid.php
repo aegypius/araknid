1745d15d49f08b2967fb2fdf9f899669779e4cdb
aegypius <innercircle@aegypius.com>
2010-04-29 15:32:17 +0200

    Basic routing class is working

--- libs/Araknid.php
+++ libs/Araknid.php
@@ -1,4 +1,5 @@
 <?php
+defined('PHP_EXT') or define('PHP_EXT', '.' . pathinfo(__FILE__, PATHINFO_EXTENSION));
 
 class Araknid {
 	const VERSION = "0.1";
@@ -12,6 +13,7 @@ class Araknid {
   Public:
 -------------------------------------------------------------------- */
 	public function __construct() {
+		debug('Initializing '.__CLASS__);
 		$this->request = Araknid_Request::getInstance();
 		$this->router  = Araknid_Router::getInstance();
 	}
@@ -40,7 +42,7 @@ if ((!defined('ARAKNID_AUTOLOAD')) || (ARAKNID_AUTOLOAD == true)) {
 	function __autoload($className) {
 		$classFile = join(DIRECTORY_SEPARATOR, array_map('ucfirst', explode('_', $className)))
 			   . (!defined('PHP_EXT') ? '.php' : PHP_EXT);
-		include $classFile;
+		@include $classFile;
 	}
 
 	/* Initializing Log System ------------------------------------- */
--- libs/Araknid/Router.php
+++ libs/Araknid/Router.php
@@ -2,10 +2,88 @@
 
 class Araknid_Router {
 	static $instance;
-	protected $routes;
+	static $candidates = false;
+	protected $routes = array(
+		':module/:controller/:action',
+		':module/:controller',
+		':module',
+		':controller/:action',
+		':controller'
+	);
+	protected $defaults = array(
+		'module' => null,
+		'controller' => 'main',
+		'action' => 'index'
+	);
 
 	public function __construct() {
 		$request = Araknid_Request::getInstance();
+		debug('Initializing '.__CLASS__);
+		$this->initController($this->route($request));
+	}
+
+	protected function route($request) {
+		debug(__CLASS__ . ' > Routing : ' . $request);
+		$path = join(DIRECTORY_SEPARATOR, array(APP_PATH, 'controllers'));
+
+		if (self::$candidates === false) {
+			$items = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path), RecursiveIteratorIterator::SELF_FIRST);
+			foreach ($items as $item) {
+				if ($item->isFile() && strpos(strtolower($item->getFilename()), PHP_EXT) !== false)
+					self::$candidates[] = $item;
+			}
+		}
+		$items = self::$candidates;
+
+		foreach ($this->routes as $route) {
+			$segs = explode('/', $route);
+			foreach ($segs as &$seg) {
+				switch ($seg) {
+					case ':module'     : $seg = '(?<module>[^\/]+)';     break;
+					case ':controller' : $seg = '(?<controller>[^\/]+)'; break;
+					case ':action'     : $seg = '(?<action>[^\/]+)?';     break;
+				}
+			}
+			$route = '/^\/' . implode('\/', $segs). '/';
+
+			debug(__CLASS__ . ' > Route : ' . $route);
+			if (preg_match($route, strtolower($request), $m)) {
+				// cleaning matches ugly way
+				for ($i = 0; $i < count($m); $i++) unset($m[$i]);
+				$m = array_merge($this->defaults, $m);
+
+				list($module, $controller, $action) = array_values($m);
+
+				$classname = ucfirst(strtolower($controller)) . 'Controller';
+				$filename = $path . DIRECTORY_SEPARATOR . trim(join(DIRECTORY_SEPARATOR, array($module, $controller)) . PHP_EXT, DIRECTORY_SEPARATOR);
+				
+
+				$found = false;
+				foreach ($items as $item) {
+					if (strtolower($item->getPathname()) == $filename) include_once $item->getPathname();
+					try {
+						$C = new ReflectionClass($classname);
+						if (!$C->hasMethod($action)) continue;
+						return array($classname, $action);
+					} catch (ReflectionException $e) {}
+				}
+			}
+		}
+		return false;
+	}
+
+	
+	
+	protected function initController($callback) {
+		if ($callback == false) {
+			header('HTTP/1.1 404 Not Found');
+			print 'Not Found';
+			return;
+		}
+		list($class, $method) = $callback;
+		$R = new ReflectionClass($class);
+		$C = $R->newInstance();
+		$C->$method();
 	}
 
 	static function getInstance() {
