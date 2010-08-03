07302613ddc64fa341264a9fcab998f0c3d7e19d
aegypius <innercircle@aegypius.com>
2010-08-03 12:34:01 +0200

    Add custom callbacks supports, default is pear-like loader

--- libs/Araknid/Loader.php
+++ libs/Araknid/Loader.php
@@ -2,50 +2,79 @@
 
 class Araknid_Loader {
 
-	public function controller($class, $data = array()) {
-	}
+   // Protected : ------------------------------------------------------
+   protected $callbacks = array();
 
-	public function view($view) {
-		$paths = explode(PATH_SEPARATOR, APP_VIEWS_PATH);
-		foreach ($paths as $path) {
-			$f = join('/', array($path, $view));
-			if (!strpos(PHP_EXT, $view)) $f .= PHP_EXT;
-			if (file_exists($f))
-				return $f;
-		}
-		throw new Exception("View Not Found : $view");
-	}
+   protected function __construct() {
+      // Register default callback
+      $this->callbacks[] = array(&$this, 'pear_loader');
+   }
 
-	static function loader($class) {
-		// Retrieving include path
-		$path     = explode(PATH_SEPARATOR, dirname(__FILE__) . PATH_SEPARATOR . get_include_path());
+   protected function pear_loader($classname) {
+      // Retrieving include path
+      $path     = explode(PATH_SEPARATOR, dirname(__FILE__) . PATH_SEPARATOR . get_include_path());
 
-		// Convert path to absolute paths
-		foreach ($path as &$p)
-			$p    = realpath($p);
+      // Convert path to absolute paths
+      foreach ($path as &$p)
+         $p    = realpath($p);
 
-		// Convert classname to path
-		$ext      = (defined('PHP_EXT') ? PHP_EXT : pathinfo(__FILE__, PATHINFO_EXTENSION));
-		$filepath = implode(DIRECTORY_SEPARATOR, explode('_', $class)) . $ext;
+      // Convert classname to path
+      $ext      = (defined('PHP_EXT') ? PHP_EXT : pathinfo(__FILE__, PATHINFO_EXTENSION));
+      $filepath = implode(DIRECTORY_SEPARATOR, explode('_', $classname)) . $ext;
 
-		// Let's search class file without case-sensitivness
-		foreach ($path as $p) {
-			$items = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($p), RecursiveIteratorIterator::SELF_FIRST);
-			foreach ($items as $item) {
-				if ($item->isFile() && (strtolower($p . DIRECTORY_SEPARATOR . $filepath) == strtolower($item->getRealpath()))) {
-					include $item->getRealpath();
-					if (class_exists($class))
-						return;
-				}
-			}
-		}
-	}
+      // Let's search class file without case-sensitivness
+      foreach ($path as $p) {
+         $items = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($p), RecursiveIteratorIterator::SELF_FIRST);
+         foreach ($items as $item) {
+            if ($item->isFile() && (strtolower($p . DIRECTORY_SEPARATOR . $filepath) == strtolower($item->getRealpath()))) {
+               include $item->getRealpath();
+               if (class_exists($classname))
+                  return;
+            }
+         }
+      }
+   }
 
-	static $instance;
-	static function getInstance() {
-		if (!isset(self::$instance))
-			self::$instance = new self();
-		return self::$instance;
-	}
+   // Public : ------------------------------------------------------
+   public function controller($class, $data = array()) {
+
+   }
+
+
+   public function view($view) {
+      $paths = explode(PATH_SEPARATOR, APP_VIEWS_PATH);
+      foreach ($paths as $path) {
+         $f = join('/', array($path, $view));
+         if (!strpos(PHP_EXT, $view)) $f .= PHP_EXT;
+         if (file_exists($f))
+            return $f;
+      }
+      throw new Exception("View Not Found : $view");
+   }
+
+
+   // Static : ---------------------------------------------------------
+   static function & register($callback) {
+      $callbacks =& self::getInstance()->callbacks;
+      if (!is_callable($callback))
+         throw new Exception('Argument is not a valid callback.');
+      if (!in_array($callback, $callbacks))
+         $callbacks[] = $callback;
+      return self::getInstance();
+   }
+
+   static function loader($class) {
+      reset(self::getInstance()->callbacks);
+      while (!class_exists($class) && (list(,$callback) = each(self::getInstance()->callbacks))) {
+          call_user_func_array($callback, array($class));
+      }
+   }
+
+   static $instance;
+   static function & getInstance() {
+      if (!isset(self::$instance))
+         self::$instance = new self();
+      return self::$instance;
+   }
 
 }
